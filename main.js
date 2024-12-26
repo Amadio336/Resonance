83c28d69f2ecfa7d11c093d9e4b6b71eb2e84311
Amadio336 <Amadio.lombardozzi@gmail.com>
2024-12-26 21:45:52 +0100

    primo abbozzo di app coesa

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+/* phase 1 */
+
+
 /*  take the elements */
 const inputGtx = document.getElementById("input-gtx")
 const buttonSubmit = document.getElementById("button-submit")
@@ -5,7 +8,7 @@ const buttonSubmit = document.getElementById("button-submit")
 
 
 
-/* creation of El on buttonSubmit */
+/* creation of El on buttonSubmit. it creates a function that cleans the greek text */
 buttonSubmit.addEventListener("click", handleGtx)
 
 
@@ -13,8 +16,72 @@ buttonSubmit.addEventListener("click", handleGtx)
 /* function handleGtx */
 function handleGtx(e) {
     e.preventDefault()
-   let rowGtx = inputGtx.value
-   console.log(rowGtx)
-   let noCommaGtx = rowGtx.replace(/,/g, " ")
-   console.log(noCommaGtx)     
+
+     
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* phase 3 */
+const resonanceElements = document.querySelectorAll(".resonance-element")
+const dragAreas = document.querySelectorAll(".drag-area")
+let dragItem = null
+
+
+
+/* creation of El on all the resonance element and related functions */
+
+resonanceElements.forEach(resonanceElement => {
+    resonanceElement.addEventListener("dragstart", handleDragStart)
+    }) 
+
+
+
+function handleDragStart(){
+  
+    this.classList.add("already-used")
+    dragItem = this
+    console.log(dragItem)
+
+}
+
+
+
+
+
+/* creation of El on all the drag areas and related functions */
+
+dragAreas.forEach(dragArea => {
+    dragArea.addEventListener("dragenter", handleDragEnter)
+    dragArea.addEventListener("dragover", handleDragOver)
+    dragArea.addEventListener("drop", handleDrop)
+})
+
+function handleDragEnter(e) {
+    console.log("element entered")
+    
+}
+
+function handleDragOver(e) {
+    e.preventDefault()
+    console.log("dragover")
+    
+}
+
+function handleDrop() {
+    this.append(dragItem)
+    
+}
+
