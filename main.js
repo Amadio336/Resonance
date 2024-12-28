bcdf6064a82c91903bfe9e22560a4830e08abc25
Amadio336 <Amadio.lombardozzi@gmail.com>
2024-12-28 22:31:13 +0100

    quasi finita la tabella

--- main.js
+++ main.js
@@ -16,6 +16,9 @@ const makeDiagraph = document.getElementById("make-diagraph")
 
 const sidebarResonanceElements = document.getElementById("main-sidebar")
 
+const tableDiagraph = document.getElementById("table-diagraph")
+
+
 
 let dragItem = null
 
@@ -98,22 +101,6 @@ function createElementForDiagraph() {
 
 
 
-
-
-
-
-
-
-
-
-
-const dragAreas = document.querySelectorAll(".drag-area")
-
-
-
-
-
-
 function handleDragStart(){
     console.log("funziona")
     this.classList.add("already-used")
@@ -126,27 +113,125 @@ function handleDragStart(){
 
 
 
-/* creation of El on all the drag areas and related functions */
 
-dragAreas.forEach(dragArea => {
-    dragArea.addEventListener("dragenter", handleDragEnter)
-    dragArea.addEventListener("dragover", handleDragOver)
-    dragArea.addEventListener("drop", handleDrop)
+
+
+let row = null
+let columnsPerRow = null
+
+
+
+//  table tool and costruction of table
+
+cells = document.querySelectorAll(".cell") // selection of the cells
+
+cells.forEach(cell => {
+    cell.addEventListener("click", () =>{    //function that takes the data value 
+       row = cell.getAttribute("data-row")
+       columnsPerRow = cell.getAttribute("data-column-per-row")
+       console.log(row + columnsPerRow)
+
+
+    
+
+       
+       for (let i = 0; i < row; i++) {   // loops that create table
+        tr = document.createElement("tr")
+        tableDiagraph.appendChild(tr)
+
+     
+        for (let i = 0; i < columnsPerRow; i++) {
+            const td = document.createElement("td")
+            td.classList.add("drag-area")
+            const resonanceElementPlacheHolder = document.createElement("p")
+            resonanceElementPlacheHolder.textContent = ""
+            tr.appendChild(td)
+            td.appendChild(resonanceElementPlacheHolder)
+
+
+
+                /* creation of El on all the drag areas and related functions */
+               const dragAreas = document.querySelectorAll(".drag-area")
+
+              dragAreas.forEach(dragArea => {
+                      dragArea.addEventListener("dragenter", handleDragEnter)
+                      dragArea.addEventListener("dragover", handleDragOver)
+                      dragArea.addEventListener("drop", handleDrop)
+                  })
+  
+                      function handleDragEnter(e) {
+                          e.preventDefault()
+                          console.log("element entered") 
+                      }
+  
+                      function handleDragOver(e) {
+                          e.preventDefault()
+                          console.log("dragover") 
+                      }
+  
+                      function handleDrop() {
+                          this.append(dragItem)
+                      } 
+        
+        
+        
+        
+        }
+
+          
+    }
+        
+    
+
+
+
+
 })
 
-function handleDragEnter(e) {
-    console.log("element entered")
-    
-}
 
-function handleDragOver(e) {
-    e.preventDefault()
-    console.log("dragover")
     
-}
+})
 
-function handleDrop() {
-    this.append(dragItem)
-    
-}
+
+
+
+/* mouse effects on table tool */
+
+cells.forEach(cell => {
+    cell.addEventListener("mouseover", () => {
+        dataRow = cell.getAttribute("data-row")
+        dataColumnsPerRow = cell.getAttribute("data-column-per-row")
+
+        for (const cell of cells) {
+            if (cell.getAttribute("data-row") <= dataRow && cell.getAttribute("data-column-per-row") <= dataColumnsPerRow) {
+                cell.classList.add("cell-mouseover")
+                
+            }
+            
+        }
+        
+        
+    })
+
+
+    cell.addEventListener("mouseout", () => {
+        dataRow = cell.getAttribute("data-row")
+        dataColumnsPerRow = cell.getAttribute("data-column-per-row")
+
+        for (const cell of cells) {
+            if (cell.getAttribute("data-row") <= dataRow && cell.getAttribute("data-column-per-row") <= dataColumnsPerRow) {
+                cell.classList.remove("cell-mouseover")
+                
+            }
+            
+        }
+        
+    })
+
+
+
+})
+
+
+
 
