d8b871f829e6be0cabcd911f7f782ac7e43cddb5
Amadio336 <Amadio.lombardozzi@gmail.com>
2025-06-02 14:47:11 +0200

    stato soddisfacente ricerca automatica

--- automatic-research.js
+++ automatic-research.js
@@ -2,6 +2,51 @@ import { sortedArr } from "./APIs.js"
 
 const buttonAutomaticResearch = document.getElementById("automatic-research")
 
+const colours = [
+    'red', 'green', 'blue', 'yellow', 'purple',
+    'orange', 'pink', 'brown', 'black', 'white',
+    'grey', 'cyan', 'magenta', 'lime', 'indigo',
+    'violet', 'turquoise', 'beige', 'maroon', 'gold', 'red', 'green', 'blue', 'yellow', 'purple',
+    'orange', 'pink', 'brown', 'black', 'white',
+    'grey', 'cyan', 'magenta', 'lime', 'indigo',
+    'violet', 'turquoise', 'beige', 'maroon', 'gold',
+    'red', 'green', 'blue', 'yellow', 'purple',
+    'orange', 'pink', 'brown', 'black', 'white',
+    'grey', 'cyan', 'magenta', 'lime', 'indigo',
+    'violet', 'turquoise', 'beige', 'maroon', 'gold', 'red', 'green', 'blue', 'yellow', 'purple',
+    'orange', 'pink', 'brown', 'black', 'white',
+    'grey', 'cyan', 'magenta', 'lime', 'indigo',
+    'violet', 'turquoise', 'beige', 'maroon', 'gold',
+    'red', 'green', 'blue', 'yellow', 'purple',
+    'orange', 'pink', 'brown', 'black', 'white',
+    'grey', 'cyan', 'magenta', 'lime', 'indigo',
+    'violet', 'turquoise', 'beige', 'maroon', 'gold', 'red', 'green', 'blue', 'yellow', 'purple',
+    'orange', 'pink', 'brown', 'black', 'white',
+    'grey', 'cyan', 'magenta', 'lime', 'indigo',
+    'violet', 'turquoise', 'beige', 'maroon', 'gold',
+    'red', 'green', 'blue', 'yellow', 'purple',
+    'orange', 'pink', 'brown', 'black', 'white',
+    'grey', 'cyan', 'magenta', 'lime', 'indigo',
+    'violet', 'turquoise', 'beige', 'maroon', 'gold', 'red', 'green', 'blue', 'yellow', 'purple',
+    'orange', 'pink', 'brown', 'black', 'white',
+    'grey', 'cyan', 'magenta', 'lime', 'indigo',
+    'violet', 'turquoise', 'beige', 'maroon', 'gold', 'red', 'green', 'blue', 'yellow', 'purple',
+    'orange', 'pink', 'brown', 'black', 'white',
+    'grey', 'cyan', 'magenta', 'lime', 'indigo',
+    'violet', 'turquoise', 'beige', 'maroon', 'gold', 'red', 'green', 'blue', 'yellow', 'purple',
+    'orange', 'pink', 'brown', 'black', 'white',
+    'grey', 'cyan', 'magenta', 'lime', 'indigo',
+    'violet', 'turquoise', 'beige', 'maroon', 'gold',
+    'red', 'green', 'blue', 'yellow', 'purple',
+    'orange', 'pink', 'brown', 'black', 'white',
+    'grey', 'cyan', 'magenta', 'lime', 'indigo',
+    'violet', 'turquoise', 'beige', 'maroon', 'gold', 'red', 'green', 'blue', 'yellow', 'purple',
+    'orange', 'pink', 'brown', 'black', 'white',
+    'grey', 'cyan', 'magenta', 'lime', 'indigo',
+    'violet', 'turquoise', 'beige', 'maroon', 'gold',
+];
+
+
 
 let colorIndex = 0
 
@@ -12,45 +57,83 @@ let index = 0
 
 
 function prova() {
-    const words = document.querySelectorAll(".highlightable")
+    const words = document.querySelectorAll(".highlightable") /*  the elements in the text provided by user */
 
     sortedArr.forEach(element => {
         SubVoceOnlyArr.push(element.SubVoce)
     })
 
-    console.log(SubVoceOnlyArr)
+
 
     SubVoceOnlyArr.forEach(element => {
-        let n = SubVoceOnlyArr.indexOf(element, index)
+        let n = SubVoceOnlyArr.indexOf(element, index) /* n is the index of element in the SubVoceOnlyArr */
+
+        const RepMap = {}
 
 
 
-        for (let i = 1; i < 15; i++) {
+        for (let i = 1; i < 20; i++) {
+            /* 
+                        console.log(SubVoceOnlyArr[n], "    ", SubVoceOnlyArr[n + i]) */
 
-/*             console.log("n", n, "    ", "n+i", n + i) */
-
-            if (element == SubVoceOnlyArr[n + i]) {
+            if (element == SubVoceOnlyArr[n + i]) {  /* the logic is -> if an element is equal to a next element, make colored the repetitions */
                 console.log("giusto")
 
-                const colours = ['green', 'blue', 'yellow', 'purple'];
 
-                words[n].style.backgroundColor = colours[colorIndex]
-                words[n + i].style.backgroundColor = colours[colorIndex]
-                colorIndex++
+                console.log("n", n, "     ", "n+i", n + i, SubVoceOnlyArr[n])
 
-
+                words[n].setAttribute("data-repetition", SubVoceOnlyArr[n])
+                words[n + i].setAttribute("data-repetition", SubVoceOnlyArr[n + i])
 
 
+                RepMap.value = SubVoceOnlyArr[n]
+                RepMap.colour = colours[colorIndex]
 
-            }
 
-            index = n + 1
-        }
+
+
+                words[n].classList.add("repetition")
+                words[n + i].classList.add("repetition")
+
+
+                
+                
+                
+                
+                
+                
+                /*       words[n].style.backgroundColor = colours[colorIndex]
+                     words[n + i].style.backgroundColor = colours[colorIndex]  */
+                     
+                    }
+                    index = n + 1
+                    
+                }
+                
+                const repetitions = document.querySelectorAll(".repetition")
+                /*        console.log("repetition", repetitions) */
+                
+                repetitions.forEach(repetition => {
+                    if (repetition.getAttribute("data-repetition") == RepMap.value) {
+                        repetition.style.color = RepMap.colour
+                        colorIndex++
+                        
+                    }
+                    
+                    
+                })
+                
+                
+                
+
+
 
     })
-
+    
+        console.log("colorIndex", colorIndex) 
 
 }
+
 
 
 
