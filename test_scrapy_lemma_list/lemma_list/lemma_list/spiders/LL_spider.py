e0156011cb09ec0c7ce2f57ce582f41606811a55
Amadio336 <Amadio.lombardozzi@gmail.com>
2025-08-12 09:36:54 +0200

    iniziato la stesura del dizionario

--- test_scrapy_lemma_list/lemma_list/lemma_list/spiders/LL_spider.py
+++ test_scrapy_lemma_list/lemma_list/lemma_list/spiders/LL_spider.py
@@ -1,6 +1,11 @@
 import scrapy
 
 
+""" You analysed from page 1 to page 800 of Perseus Lemma List. You must check if in thwe different json file there are copies. In the single json files there
+are copies.
+
+to use the program, just modfy the numbers in range at the end of the code """
+
 class LlSpiderSpider(scrapy.Spider):
     name = "LL_spider"
     allowed_domains = ["vocab.perseus.org"]
@@ -11,7 +16,7 @@ class LlSpiderSpider(scrapy.Spider):
         counts_not_sorted = response.xpath("//td[@class='count']/text()").getall()
         counts_sorted = []
 
-        for index in range(int(len(counts_not_sorted)/4)):
+        for index in range(int(len(counts_not_sorted)/4)): # that is: for 20 times, because the total words are 20, multiplied for 4 counts for every word
             counts_sorted.append([counts_not_sorted[0], counts_not_sorted[1], counts_not_sorted[2], counts_not_sorted[3]])
             counts_not_sorted = counts_not_sorted[4:]
 
@@ -32,6 +37,14 @@ class LlSpiderSpider(scrapy.Spider):
 
         base_link = "https://vocab.perseus.org/lemma/"
         mover = "?page="
+
+        for page_index in range(601, 800):
+            next_page_url = f"{base_link}{mover}{page_index}"
+            yield response.follow(next_page_url, callback = self.parse)
+
+
+
+
         
         
         
