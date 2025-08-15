e7420b5ecab11a6e75441a42dd4f16989ff843d1
Amadio336 <Amadio.lombardozzi@gmail.com>
2025-08-15 17:37:56 +0200

    correction

--- adding_traslations/json_interpreter.py
+++ adding_traslations/json_interpreter.py
@@ -1,11 +1,11 @@
 import json 
 
-dict = None
+gk_dict = None
 data_to_modify = None
 
 try:
     with open("dict.json", "r") as file:
-        dict = json.load(file)
+        gk_dict = json.load(file)
 
     with open("data_to_modify.json", "r") as file2:
         data_to_modify = json.load(file2)
@@ -20,7 +20,7 @@ for element_to_modify in data_to_modify:
 
 
 for element_to_modify in data_to_modify:
-    for element_dict in dict:
+    for element_dict in gk_dict:
         if element_to_modify["SubVoce"] == element_dict["SubVoce"]:
             element_to_modify["tras"] = element_dict["tras"]
 
